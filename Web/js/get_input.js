886e36cb913dec59a6ac72ec923a38c6f99efa90
QuentinChapuzet <qu.chapuzet@gmail.com>
2021-11-04 17:04:36 +0100

    update fonction de lecture fichier

--- Web/js/get_input.js
+++ Web/js/get_input.js
@@ -20,3 +20,28 @@ function getInputValue() {
       // Displaying the value
       console.log(val);
   }
+
+//////// PAS REUSSI A LIRE LES DONNES CONTENUES DANS UN FICHIER TEXTE ///////////// LA fonction semble bien 
+
+  function lireFichierTexte(fichier)
+  {
+   //On lance la requête pour récupérer le fichier
+   var fichierBrut = new XMLHttpRequest();
+   fichierBrut.open("GET", fichier, false);
+   //On utilise une fonction sur l'événement "onreadystate"
+   fichierBrut.onreadystatechange = function ()
+   {
+   if(fichierBrut.readyState === 4)
+        {
+   //On contrôle bien quand le statut est égal à 0
+        if(fichierBrut.status === 200 || fichierBrut.status == 0)
+            {
+            //On peut récupérer puis traiter le texte du fichier
+            var texteComplet = fichierBrut.responseText;
+            alert(texteComplet);
+            console.log(texteComplet);
+            }
+        }
+   }
+   fichierBrut.send(null);
+  }
